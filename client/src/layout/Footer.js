a77c285901ad62aca75a4aaad839571f434e2f83
Kishor978 <thaunnakishor@gmail.com>
2024-12-26 16:50:41 +0545

    client  folder updated

---
+++ client/src/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='center'>
+        <p>Copyright By &copy; Kishor</p>
+    </footer>
+  )
+}
+
+export default Footer
