47a998812d595cdb753c172523431b0f9186650f
Kishor978 <thaunnakishor@gmail.com>
2024-12-28 19:07:16 +0545

    registration and login

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import {Home, About, Register, Login,Error} from '../pages'
+import {Home, About, Register, Login,Error, Dashboard} from '../pages'
 import NavBar from '../layout/NavBar'
 import Footer from '../layout/Footer'
 import Activate from '../components/Activate'
-
+import Protected from "./ProtectRoutes"
 
 const Index = () => {
   return (
@@ -16,6 +16,9 @@ const Index = () => {
           <Route path="/about" element={<About/>} /> 
           <Route path="/register" element={<Register/>} /> 
           <Route path="/login" element={<Login/>} /> 
+          <Route path="/dashboard" element={<Protected/>} >
+            <Route path="" element={<Dashboard/>} />
+          </Route> 
           <Route path="/auth/activate/:jwtToken" element={<Activate/>} /> 
           <Route path="*" element={<Error/>} /> 
         </Routes>
